e1ecdfbcb8c3de9e4a2c0a6cfd5d60a15d63dfd5
ArthurBandeira12 <arthurbandeiralealdeoliveira@gmail.com>
2025-10-08 18:14:24 -0300

    adicionado factory, seeds e models

---
+++ atividade_01/database/factories/BookFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Category;
+use App\Models\Publisher;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * 
+     */
+    protected $model = Book::class;
+    
+    public function definition(): array
+    {
+        return [
+            'title'=>$this->faker->sentence(),
+            'author_id'=> Author::factory(),
+            'category_id'=> Category::factory(),
+            'publisher_id'=> Publisher::factory(),
+            'published_year'=> $this->faker->year,
+            'pages' => $this->faker->numberBetween(100, 1000),
+        ];
+    }
+}
